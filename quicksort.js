cf6cf3fa239a41cff45d8a506aeb5c08582625bb
Dvwhite81 <123031200+Dvwhite81@users.noreply.github.com>
2023-08-08 23:42:07 -0400

    Add files via upload

---
+++ quicksort.js
@@ -0,0 +1,33 @@
+function quicksort(arr) {
+
+  // Check if the input is length 1 or less
+  if (arr.length <= 1) {
+    // If so, it's already sorted: return
+    return arr;
+  }
+
+  // Pick the first value as the pivot
+  let pivot = arr[0];
+  // Orient the pivot so that...
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      // every number smaller than the pivot is to the left
+      left.push(arr[i]);
+    } else {
+      // every number larger (or equal) than the pivot is to the right
+      right.push(arr[i]);
+    }
+
+  // Recursively sort the left
+  left = quicksort(left);
+  // Recursively sort the right
+  right = quicksort(right);
+  }
+  // Return the left, pivot and right in sorted order
+  let results = [...left, pivot, ...right];
+  return results;
+}
+
+module.exports = [quicksort];
